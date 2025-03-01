920a64a8e6bdc201ac878aae2561ed2ba439e3d3
Obe Ajilima <ajilimaobe@gmail.com>
2025-03-01 13:35:58 +0100

    added minimized task and expanded task forms

--- src/todo.js
+++ src/todo.js
@@ -1,12 +1,16 @@
+// Form Visibility Module
+
 export function formVisibility () {
-    let addTodo = document.querySelector('.add');
+    let addTodo = document.querySelector('.add'); // get "Add Todo" button
     let form = document.getElementById('popUpContainer');
     let cancel = document.getElementById('cancelBtn');
 
+    // display the form when the "Add Todo" button is clicked
     addTodo.addEventListener('click', () => {
         form.style.display = 'flex';
     });
 
+    // hide the form when the "Cancel" button is clicked
     cancel.addEventListener('click', () => {
         form.style.display = 'none';
     });
